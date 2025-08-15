5e83937818425d9b135c4a74a5ef5642eb9a5eb7
abhilashmendhe <abhilashmendhe@gmail.com>
2025-08-15 11:46:21 +0530

    created route user logout

---
+++ _2_personal_blogging/src/routes/users/logout.rs
@@ -0,0 +1,24 @@
+use axum::{extract::State, http::StatusCode, Extension};
+use sqlx::PgPool;
+
+use crate::{models::user::UserDB, utils::errors::AppError};
+
+pub async fn logout(
+    Extension(user): Extension<UserDB>,
+    State(db): State<PgPool>
+) -> Result<StatusCode, AppError> {
+
+    sqlx::query(r#"UPDATE users SET token=$1 WHERE id=$2"#)
+        .bind("")
+        .bind(user.id)
+        .execute(&db)
+        .await
+        .map_err(|err| {
+            eprint!("Error user logout: {:?}",err);
+            AppError::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "There was problem logging out..."
+            )
+        })?;
+    Ok(StatusCode::OK)
+}
