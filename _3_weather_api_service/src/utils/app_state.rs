25c1870aadbf869c2ae1af028eda2da9c4bdb5a2
abhilashmendhe <abhilashmendhe@gmail.com>
2025-08-29 13:04:01 +0530

    added redis connection to the app state

--- _3_weather_api_service/src/utils/app_state.rs
+++ _3_weather_api_service/src/utils/app_state.rs
@@ -1,19 +1,21 @@
 use std::sync::{atomic::AtomicU32, Arc};
 
 use axum::extract::FromRef;
+use redis::{aio::MultiplexedConnection};
 
 use crate::utils::config::Config;
 
 #[derive(Debug, Clone)]
 pub struct AppState {
     pub visit_count: Arc<AtomicU32>,
-    pub config: Config
+    pub config: Config,
+    pub conn: MultiplexedConnection
 }
 
 impl AppState {
-    pub fn new(config: Config) -> Self {
+    pub fn new(config: Config, conn: MultiplexedConnection) -> Self {
         let visit_count = Arc::new(AtomicU32::new(0));
-        AppState { visit_count, config }
+        AppState { visit_count, config, conn }
     }
 }
 
@@ -26,5 +28,11 @@ impl FromRef<AppState> for Config {
 impl FromRef<AppState> for Arc<AtomicU32> {
     fn from_ref(state: &AppState) -> Self {
         state.visit_count.clone()
+    }
+}
+
+impl FromRef<AppState> for MultiplexedConnection {
+    fn from_ref(state: &AppState) -> Self {
+        state.conn.clone()
     }
 }
