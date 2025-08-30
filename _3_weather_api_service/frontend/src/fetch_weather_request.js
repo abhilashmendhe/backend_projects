742f5bebc254415c5ab61333d24e8b01d3b85611
abhilashmendhe <abhilashmendhe@gmail.com>
2025-08-30 16:14:16 +0530

    added an alert if too many requets were passed

--- _3_weather_api_service/frontend/src/fetch_weather_request.js
+++ _3_weather_api_service/frontend/src/fetch_weather_request.js
@@ -1,8 +1,12 @@
 export async function fetchWeather(url) {
   try {
     const data = await fetch(url); // Wait for the promise to resolve and assign the result
-    const weatherData = await data.json();
-    return weatherData;
+    // console.log("Status:",data.status);
+    if (data.status===200) {
+      const weatherData = await data.json();
+      return weatherData;
+    } 
+    alert("Too many reqests. Wait for 60 seconds.");
   } catch (error) {
     console.error("Failed to fetch the data", error);
   }
