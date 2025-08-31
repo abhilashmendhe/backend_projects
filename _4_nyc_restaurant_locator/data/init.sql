05a3293bc93ad72ec7c9d5869f310614991836a6
abhilashmendhe <abhilashmendhe@gmail.com>
2025-08-31 16:31:50 +0530

    started with postgis db. Created schema for my project

---
+++ _4_nyc_restaurant_locator/data/init.sql
@@ -0,0 +1,29 @@
+CREATE EXTENSION IF NOT EXISTS dblink;
+
+DO
+$$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_database WHERE datname = 'restaurant_db'
+   ) THEN
+      PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE restaurant_db');
+   END IF;
+END
+$$;
+
+\connect restaurant_db;
+
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+CREATE TABLE IF NOT EXISTS nyc_restaurants(
+    id SERIAL PRIMARY KEY,
+    url TEXT,
+    name TEXT,
+    rating NUMERIC,
+    rating_count INTEGER,
+    detailed_ratings JSONB,
+    price_category INTEGER,
+    address TEXT,
+    location GEOMETRY(Point, 4326),
+    zipcode INTEGER
+);
