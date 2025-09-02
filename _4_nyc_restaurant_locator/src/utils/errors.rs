ac97c365d8ff46f78952b22b929150ae2272ac09
abhilashmendhe <abhilashmendhe@gmail.com>
2025-09-02 12:14:02 +0530

    added custom response for each error type

--- _4_nyc_restaurant_locator/src/utils/errors.rs
+++ _4_nyc_restaurant_locator/src/utils/errors.rs
@@ -1,6 +1,7 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+use tracing::error;
 
 #[derive(Debug, Error)]
 pub enum WebError {
@@ -36,5 +37,21 @@ impl IntoResponse for AppError {
         (   self.code, 
             Json(ErrorResponse{ error_message: self.message.clone()})
         ).into_response()
+    }
+}
+
+impl IntoResponse for WebError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            WebError::IOError(error) => {
+                error!("IO error: {:?}",error);
+                (error.to_string()).into_response()
+            },
+            WebError::AppError(app_error) => (   
+                app_error.code, 
+                Json(ErrorResponse{ error_message: app_error.message.clone() }
+            )
+        ).into_response(),
+        }
     }
 }
