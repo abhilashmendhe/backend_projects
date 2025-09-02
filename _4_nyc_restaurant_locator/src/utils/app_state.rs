af3e1dcda4433694a4bf3adb6ba5b8f7d9a1d223
abhilashmendhe <abhilashmendhe@gmail.com>
2025-09-02 09:56:06 +0530

    added utilities like errors, and app state for my web ap

---
+++ _4_nyc_restaurant_locator/src/utils/app_state.rs
@@ -0,0 +1,21 @@
+use axum::extract::FromRef;
+use sqlx::PgPool;
+
+#[derive(Debug, Clone)]
+pub struct AppState {
+    pub conn: PgPool
+}
+
+impl AppState {
+    pub fn new(conn: PgPool) -> Self {
+        Self {
+            conn
+        }
+    }
+}
+
+impl FromRef<AppState> for PgPool {
+    fn from_ref(state: &AppState) -> PgPool {
+        state.conn.clone()
+    }
+}
