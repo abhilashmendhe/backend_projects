285ccc83c3d3cc17cc67ea5e9979b8595334bbe2
abhilashmendhe <abhilashmendhe@gmail.com>
2025-09-02 14:48:02 +0530

    added query params ot my fetch method and changed the sql query

--- _4_nyc_restaurant_locator/src/routes/fetch_all_restaurants.rs
+++ _4_nyc_restaurant_locator/src/routes/fetch_all_restaurants.rs
@@ -1,5 +1,16 @@
+use axum::extract::Query;
+/*
+SELECT COUNT(*)
+FROM nyc_restaurants
+WHERE ST_DWithin(
+    location::geography,
+    ST_SetSRID(ST_Point(40.72979758784154,-73.9982958555126), 4326)::geography,
+    500  -- 50 km
+);
+*/
 use axum::Json;
 use axum::{extract::State, http::StatusCode};
+use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
 use sqlx::Row;
 use tracing::error;
@@ -7,17 +18,42 @@ use tracing::error;
 use crate::routes::ResponseData;
 use crate::{models::restaurant_db::RestaurantDB, utils::errors::{AppError, WebError}};
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct QueryParams {
+    pub lat: Option<f64>,
+    pub long: Option<f64>,
+    pub radius: Option<i32>
+}
+
 pub async fn fetch_all_restaurants(
-    State(conn): State<PgPool>
+    State(conn): State<PgPool>,
+    Query(q): Query<QueryParams>
 ) -> Result<(StatusCode, Json<ResponseData>), WebError> {
 
+    if q.lat.is_none() || q.long.is_none() || q.radius.is_none() {
+        return Err(WebError::AppError(
+            AppError::new(StatusCode::BAD_REQUEST, "Missing coordinates and radius")
+        ));
+    } 
+
+    let lat = q.lat.unwrap();
+    let long = q.long.unwrap();
+    let radius = q.radius.unwrap();
+
     let result = sqlx::query(r#"
-    select id,url,name,rating,rating_count,detailed_ratings,price_category,address,json_build_object(
+    SELECT id,url,name,rating,rating_count,detailed_ratings,price_category,address,json_build_object(
     'srid', ST_SRID(location),
     'lat', ST_Y(location),
     'lon', ST_X(location)
-) AS location, zipcode from nyc_restaurants
-    "#)
+) AS location, zipcode FROM nyc_restaurants 
+ WHERE ST_DWithin(
+    location::geography,
+    ST_SetSRID(ST_Point($1,$2), 4326)::geography,
+    $3  
+)"#)
+.bind(lat)
+.bind(long)
+.bind(radius)
     .fetch_all(&conn)
     .await
     .map_err(|err| {
