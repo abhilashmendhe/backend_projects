19e8d2ded42c61be2ba1f42a70271bf13e1eb84c
abhilashmendhe <abhilashmendhe@gmail.com>
2025-09-02 16:01:38 +0530

    added CORS layer

--- _4_nyc_restaurant_locator/src/routers.rs
+++ _4_nyc_restaurant_locator/src/routers.rs
@@ -1,13 +1,19 @@
-use axum::{http::StatusCode, routing::get, Router};
-
+use axum::{http::{self, Method, StatusCode}, routing::get, Router};
+use tower_http::cors::{Any, CorsLayer};
 use crate::{routes::fetch_all_restaurants::fetch_all_restaurants, utils::app_state::AppState};
 
 pub async fn create_router(app_state: AppState) -> Router {
 
+    // cors
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:5173".parse::<http::HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
+        .allow_headers(Any);
+
     Router::new()
 
         .route("/v1/restaurants", get(fetch_all_restaurants))
-
+        .layer(cors)
         .route("/", get(
             (StatusCode::OK, 
                 "Welcome to my NYC restaurant finder web app service!")
