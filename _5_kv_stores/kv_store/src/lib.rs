0178527c4427b96b28aea8030a4dc75b083e50a6
abhilashmendhe <abhilashmendhe@gmail.com>
2025-10-04 22:56:53 +0530

    modified lib.rs a little and created the main.rs file

--- _5_kv_stores/kv_store/src/lib.rs
+++ _5_kv_stores/kv_store/src/lib.rs
@@ -4,14 +4,16 @@ use crate::{routers::routers, utils::{app_state::AppState, errors::KVError}};
 
 pub mod routers;
 pub mod utils;
+pub mod data;
+pub mod routes;
 
-pub async fn run(app_state: AppState, addr_ip: &str) -> Result<(), KVError>{
+pub async fn run(app_state: AppState, addr: &str) -> Result<(), KVError>{
     
     let app = routers(app_state);
     
-    let listener = TcpListener::bind(addr_ip).await?;
-
+    let listener = TcpListener::bind(addr).await?;
+    println!("🚀 Timestore KV Started listening on `{}`\n", addr);
     axum::serve(listener, app).await?;
-    
+
     Ok(())
 }
--- _5_kv_stores/kv_store/src/main.rs
+++ _5_kv_stores/kv_store/src/main.rs
@@ -3,6 +3,45 @@ To run:
     $ cargo watch -q -c -w src/ -x run
 */
 
-fn main() {
+use kv_store::{run, utils::{app_state::AppState, errors::KVError}};
+use clap::Parser;
+use tracing::level_filters::LevelFilter;
+
+
+#[derive(Parser)]
+#[command(name = "KV-Store")]
+// #[command(version = "1.0")]
+#[command(about = "Timebased Key-Value Store Server", long_about = None)]
+struct Args {
+
+    /// IP Address
+    #[arg(long)]
+    ip_addr: String,
+
+    /// Port
+    #[arg(long)]
+    port: String
+}
+
+#[tokio::main]
+async fn main() -> Result<(), KVError> {
     
+    // 1. enable tracing.
+    tracing_subscriber::fmt()
+    .with_max_level(LevelFilter::DEBUG)
+    .init();
+
+    // 2. Get address and port from cmd
+    let a = Args::parse();
+
+    let address = &a.ip_addr;
+    let port = &a.port;
+    let addr = format!("{}:{}", address, port);
+
+    // 3. Initialize state
+    let app_state = AppState::new();
+
+    run(app_state, &addr).await?;
+
+    Ok(())
 }
