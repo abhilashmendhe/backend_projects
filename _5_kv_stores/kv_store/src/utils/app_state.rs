f2a1053d415c90c74181cbcfba3c975cd4c74054
abhilashmendhe <abhilashmendhe@gmail.com>
2025-10-04 22:54:29 +0530

    added the kv field to app state

--- _5_kv_stores/kv_store/src/utils/app_state.rs
+++ _5_kv_stores/kv_store/src/utils/app_state.rs
@@ -1,21 +1,35 @@
 use std::sync::{atomic::AtomicUsize, Arc};
 
 use axum::extract::FromRef;
+use tokio::sync::Mutex;
+
+use crate::data::kvt_struct::TimeBasedKV;
 
 
 #[derive(Debug, Clone)]
 pub struct AppState {
-    pub visit_count: Arc<AtomicUsize>
+    pub visit_count: Arc<AtomicUsize>, 
+    pub kv_store: Arc<Mutex<TimeBasedKV>>
 }
 
 impl AppState {
     pub fn new() -> Self {
-        Self { visit_count: Arc::new(AtomicUsize::new(0)) }
+        let tbkv = TimeBasedKV::new();
+        Self { 
+            visit_count: Arc::new(AtomicUsize::new(0)),
+            kv_store: Arc::new(Mutex::new(tbkv))
+        }
     }
 }
 
 impl FromRef<AppState> for Arc<AtomicUsize> {
     fn from_ref(state: &AppState) -> Self {
         state.visit_count.clone()
+    }
+}
+
+impl FromRef<AppState> for Arc<Mutex<TimeBasedKV>> {
+    fn from_ref(state: &AppState) -> Self {
+        state.kv_store.clone()
     }
 }
