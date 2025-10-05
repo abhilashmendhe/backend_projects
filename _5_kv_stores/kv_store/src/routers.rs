02fa112bc34ba1f80da21bd9321d66c19f7e2c2b
abhilashmendhe <abhilashmendhe@gmail.com>
2025-10-05 12:56:56 +0530

    added get and put routes for my kv-store

--- _5_kv_stores/kv_store/src/routers.rs
+++ _5_kv_stores/kv_store/src/routers.rs
@@ -1,10 +1,14 @@
 use axum::{routing::get, Router};
 
-use crate::{routes::health::get_health::health_check, utils::app_state::AppState};
+use crate::{routes::{health::get_health::health_check, timekv::{get_value::get_value, put_value::put_value}}, utils::app_state::AppState};
 
 pub fn routers(app_state: AppState) -> Router {
 
     Router::new()
+    
+        .route("/", get(get_value)
+                                        .put(put_value))
+
         .route("/health", get(health_check))
         .with_state(app_state)
 }
