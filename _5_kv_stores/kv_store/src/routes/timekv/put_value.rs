05f6abcd4787d2a44f45aed655bf1e221cca807c
abhilashmendhe <abhilashmendhe@gmail.com>
2025-10-05 12:56:32 +0530

    added an info for logging

--- _5_kv_stores/kv_store/src/routes/timekv/put_value.rs
+++ _5_kv_stores/kv_store/src/routes/timekv/put_value.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 
 use axum::{extract::State, http::StatusCode, Json};
 use tokio::sync::Mutex;
+use tracing::info;
 
 use crate::{data::kvt_struct::TimeBasedKV, routes::timekv::PutRequestKV};
 
@@ -9,7 +10,8 @@ pub async fn put_value(
     State(tkv): State<Arc<Mutex<TimeBasedKV>>>,
     Json(preqkv): Json<PutRequestKV>
 ) -> StatusCode {
-
+    let s = format!("key:`{}`, value:`{}`, timestamp:`{}`",&preqkv.key, &preqkv.value, &preqkv.timestamp);
+    info!(" ->> PUT   / {:>8}{{{}}}","",s);
     {
         let mut tkv_gaurd = tkv.lock().await;
         tkv_gaurd.set(preqkv.key, preqkv.value, preqkv.timestamp);
