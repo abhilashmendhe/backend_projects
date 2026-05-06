5a78ad289c1dcf951eda3e950f206539e5762f6c
Abhilash <abhilashmendhe@gmail.com>
2026-05-06 22:21:09 +0530

    added an enum for expense category. This is an experimental

--- _6_expense_tracker/src/utils/app_state.rs
+++ _6_expense_tracker/src/utils/app_state.rs
@@ -6,11 +6,41 @@ use sqlx::PgPool;
 
 use crate::utils::config::Config;
 
+pub enum CategoriesE {
+    Food,
+    Transport,
+    Housing,
+    Shopping,
+    Bills,
+    Entertainment,
+    Health,
+    Travel,
+    Misc,
+    UNSET
+}
+
+impl From<String> for CategoriesE {
+    fn from(value: String) -> Self {
+        match value.to_lowercase().as_str() {
+            "food" => CategoriesE::Food,
+            "transport" => CategoriesE::Transport,
+            "housing" => CategoriesE::Housing,
+            "bills" => CategoriesE::Bills,
+            "entertainment" => CategoriesE::Entertainment,
+            "health" => CategoriesE::Health,
+            "travel" => CategoriesE::Travel,
+            "misc" | "miscellaneous" => CategoriesE::Misc,
+            _ => CategoriesE::UNSET
+        }
+    }
+}
+
 pub struct AppState {
     pub visit_count: AtomicUsize,
     pub service_up: Instant,
     pub config: Config,
     pub pool: PgPool,
+    pub categories_e: CategoriesE
 }
 
 impl AppState {
@@ -20,6 +50,7 @@ impl AppState {
             service_up: Instant::now(),
             config,
             pool,
+            categories_e: CategoriesE::UNSET
         }
     }
 }
