668f84d850ccc6004dbabe14f2109e53959e27d9
Abhilash <abhilashmendhe@gmail.com>
2026-05-10 15:51:09 +0530

    updated mod.rs files

--- _6_expense_tracker/src/models/mod.rs
+++ _6_expense_tracker/src/models/mod.rs
@@ -1,1 +1,2 @@
+pub mod expenses_models;
 pub mod users_model;
--- _6_expense_tracker/src/routes/mod.rs
+++ _6_expense_tracker/src/routes/mod.rs
@@ -2,11 +2,10 @@ use actix_web::web::ServiceConfig;
 
 use crate::routes::{expenses::expense_view_factory, users::users_view_factory};
 
-pub mod users;
 pub mod expenses;
+pub mod users;
 
 pub fn sub_views_factory(app: &mut ServiceConfig) {
-    app
-        .configure(users_view_factory)
+    app.configure(users_view_factory)
         .configure(expense_view_factory);
 }
