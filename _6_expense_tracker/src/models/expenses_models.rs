d11345ed169d89af45e7d814b883250dc2d811f3
Abhilash <abhilashmendhe@gmail.com>
2026-05-11 10:37:35 +0530

    Added FromRow macro to ExpenseResponseModel

--- _6_expense_tracker/src/models/expenses_models.rs
+++ _6_expense_tracker/src/models/expenses_models.rs
@@ -1,8 +1,9 @@
 use bigdecimal::BigDecimal;
 use chrono::{DateTime, Utc};
 use serde::Serialize;
+use sqlx::prelude::FromRow;
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, FromRow)]
 pub struct ExpenseResponseModel {
     pub id: i32,
     pub user_id: i32,
@@ -14,4 +15,11 @@ pub struct ExpenseResponseModel {
     pub created_at: Option<DateTime<Utc>>,
     pub updated_at: Option<DateTime<Utc>>,
     pub expense_date: Option<DateTime<Utc>>,
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct Category {
+    pub id: Option<i32>,
+    pub name: Option<String>,
+    pub user_id: Option<i32>,
 }
