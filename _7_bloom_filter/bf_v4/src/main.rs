4485c8700572dc8ebcec098253ec7303a85a1700
Abhilash <abhilashmendhe@gmail.com>
2026-05-15 10:40:21 +0530

    Modified file for testing purposes

--- _7_bloom_filter/bf_v4/src/main.rs
+++ _7_bloom_filter/bf_v4/src/main.rs
@@ -2,26 +2,27 @@ use bf_v4::{bf::BloomFilter, errors::BFError};
 
 #[tokio::main]
 async fn main() -> Result<(), BFError> {
-    let bloom_filter = BloomFilter::spawn(10, 0.1, 4).await?;
+    let bloom_filter = BloomFilter::spawn(10, 0.1, 4, Some("./my-data/bits1.bin")).await?;
 
-    let mut handles = vec![];
-    for item in [
-        "apple",
-        "banana",
-        "mango",
-        "pineapple",
-        "chiku",
-        "peach",
-        "milk",
-        "chocolate",
-    ] {
-        let bf1 = bloom_filter.clone();
-        let t = tokio::spawn(async move {
-            bf1.insert(item).await?;
-            Ok::<(), BFError>(())
-        });
-        handles.push(t);
-    }
+    // let bloom_filter = BloomFilter::spawn(10, 0.1, 4, None).await?;
+    // let mut handles = vec![];
+    // for item in [
+    //     "apple",
+    //     "banana",
+    //     "mango",
+    //     "pineapple",
+    //     "chiku",
+    //     "peach",
+    //     "milk",
+    //     "chocolate",
+    // ] {
+    //     let bf1 = bloom_filter.clone();
+    //     let t = tokio::spawn(async move {
+    //         bf1.insert(item).await?;
+    //         Ok::<(), BFError>(())
+    //     });
+    //     handles.push(t);
+    // }
 
     for item in [
         "apple",
@@ -35,14 +36,18 @@ async fn main() -> Result<(), BFError> {
         println!("{} -> {}", item, f.1);
     }
 
-    for h in handles {
-        h.await??;
-    }
+    // for h in handles {
+    //     h.await??;
+    // }
 
-    let file_worker = tokio::spawn(async move {
-        bloom_filter.save("./bits1.bin").await
-    });
+    // let file_worker = tokio::spawn(async move { bloom_filter.save("./my-data/bits1.bin").await });
 
-    file_worker.await??;
+    // match file_worker.await {
+    //     Ok(inner) => match inner {
+    //         Ok(_) => println!("save success"),
+    //         Err(e) => println!("save error: {:?}", e),
+    //     },
+    //     Err(e) => println!("join error: {:?}", e),
+    // }
     Ok(())
 }
