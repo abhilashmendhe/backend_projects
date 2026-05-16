63fc122c2855c8ef3788a40a8ff011ed0f8dad11
Abhilash <abhilashmendhe@gmail.com>
2026-05-16 12:45:44 +0530

    Started a new project on building a tiny URL

---
+++ _8_tiny_url/databases/init.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY, 
+    username VARCHAR(64) NOT NULL UNIQUE, 
+    password VARCHAR(255) NOT NULL, 
+    email    VARCHAR(64) NOT NULL, 
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ, 
+    token TEXT
+);
+
+CREATE TABLE IF NOT EXISTS tinyurl (
+    id BIGSERIAL PRIMARY KEY,
+    user_id INT NOT NULL, 
+    short_url_code VARCHAR(10) NOT NULL, 
+    long_url TEXT NOT NULL, 
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ,
+    expired_at TIMESTAMPTZ,
+
+    CONSTRAINT fk_tinyurl_user
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+
+    UNIQUE(short_url_code)
+);
+
+CREATE INDEX idx_tinyurl_user_id ON tinyurl(user_id);
