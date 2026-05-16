1a0210c680a1ea960700352063b5b806f504375e
Abhilash <abhilashmendhe@gmail.com>
2026-05-16 16:32:25 +0530

    Created a user model for our users db table

---
+++ _8_tiny_url/tu_server/src/models/user_model.rs
@@ -0,0 +1,13 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserModel {
+    pub id: i64,
+    pub username: String,
+    pub password: String,
+    pub email: String,
+    pub created_at: Option<DateTime<Utc>>,
+    pub deleted_at: Option<DateTime<Utc>>,
+    pub token: Option<String>,
+}
