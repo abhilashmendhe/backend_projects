d33132631f49659edc99bab069f0e087ddfe0d40
Abhilash <abhilashmendhe@gmail.com>
2026-05-17 22:01:47 +0530

    added path for get_short_url

--- _8_tiny_url/tu_server/src/routes/tinyurl/mod.rs
+++ _8_tiny_url/tu_server/src/routes/tinyurl/mod.rs
@@ -4,11 +4,14 @@ use actix_web::{
 };
 
 use crate::{
-    middleware::require_auth::require_auth, routes::tinyurl::create_short_url::create_short_url,
+    middleware::require_auth::require_auth,
+    routes::tinyurl::{create_short_url::create_short_url, get_short_url::get_redirect_short_url},
 };
 
 const TURL: &'static str = "http://mybitiny.com";
 
+pub mod bloom_filter_insert;
+pub mod bloom_filter_query;
 pub mod create_short_url;
 pub mod get_short_url;
 pub mod helpers;
@@ -18,5 +21,10 @@ pub fn tinyurl_routers(app: &mut ServiceConfig) {
         web::resource("/tinyurl")
             .wrap(from_fn(require_auth))
             .route(web::post().to(create_short_url)),
+    )
+    .service(
+        web::resource("/tinyurl/{key}")
+            .wrap(from_fn(require_auth))
+            .route(web::get().to(get_redirect_short_url)),
     );
 }
