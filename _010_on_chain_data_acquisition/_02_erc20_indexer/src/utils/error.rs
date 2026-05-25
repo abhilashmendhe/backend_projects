426209d700cda4d8af1257b7746971e393718511
Abhilash <abhilashmendhe@gmail.com>
2026-05-25 21:56:55 +0530

    updated my custom error

--- _010_on_chain_data_acquisition/_02_erc20_indexer/src/utils/error.rs
+++ _010_on_chain_data_acquisition/_02_erc20_indexer/src/utils/error.rs
@@ -5,10 +5,19 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum TokenIndexerErr {
-    
     #[error("{}", .0)]
     EnvVarReadErr(#[from] VarError),
 
     #[error("{}", .0)]
     RpcConnectError(#[from] RpcError<TransportErrorKind>),
+    // #[error("{}", .0)]
+    // FromAlloyErr(#[from] Error),
+    #[error("{}", .0)]
+    BlockNotFound(String),
+
+    #[error("{}", .0)]
+    LogNotFound(String),
+
+    #[error("{}", .0)]
+    FailedToDecodeLog(String),
 }
