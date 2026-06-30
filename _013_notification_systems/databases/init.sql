273499cc6d0c8c93a14b1370c6fc3a042f34e805
Abhilash <abhilashmendhe@gmail.com>
2026-06-30 16:13:17 +0530

    Created a basic DB schema for users and devices. Also created a docker compose yml file

---
+++ _013_notification_systems/databases/init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    email    VARCHAR(64) NOT NULL, 
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+);
+
+CREATE TABLE IF NOT EXISTS devices (
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    device_token TEXT NOT NULL,
+    platform VARCHAR(8) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    last_seen_at TIMESTAMPTZ, 
+    is_active BOOLEAN,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
