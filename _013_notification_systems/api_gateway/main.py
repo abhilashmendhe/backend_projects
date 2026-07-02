1c2475274536a211b21b5402bc59844475d6fbf1
Abhilash <abhilashmendhe@gmail.com>
2026-07-02 11:48:54 +0530

    updated main.py added routers for http requests

--- _013_notification_systems/api_gateway/main.py
+++ _013_notification_systems/api_gateway/main.py
@@ -1,14 +1,11 @@
 # $ uvicorn main:app --host 0.0.0.0 --port 8080 --reload
 
-
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from contextlib import asynccontextmanager
-import httpx
-import os
-
-PORT = int(os.environ.get("PORT", "8080"))
-GATEWAY_URL = os.environ.get("GATEWAY_URL", "http://localhost:9000")
+from routes.users.users import router as user_router
+from routes.devices.devices import router as device_router
+from config import settings
 
 @asynccontextmanager
 async def server_lifespan(app: FastAPI):
@@ -21,3 +18,6 @@ app = FastAPI(lifespan=server_lifespan)
 @app.get("/about")
 async def about():
     return {"message":"I am API gateway for notification systems/services"}
+
+app.include_router(user_router)
+app.include_router(device_router)
