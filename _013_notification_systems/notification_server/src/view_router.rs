59e2a069a7aba0d0c5e5a099e194d2ecc732b8dc
Abhilash <abhilashmendhe@gmail.com>
2026-07-04 16:13:52 +0530

    updated view router

--- _013_notification_systems/notification_server/src/view_router.rs
+++ _013_notification_systems/notification_server/src/view_router.rs
@@ -1,16 +1,20 @@
 use actix_web::{
-    HttpResponse, Responder,
+    HttpRequest, HttpResponse, Responder,
     web::{self, ServiceConfig},
 };
 use serde::{Deserialize, Serialize};
 
-use crate::utils::app_state::AppState;
+use crate::{routes::view_routers, utils::app_state::AppState};
 
 pub fn views_factory(app: &mut ServiceConfig) {
-    app.route("/about", web::get().to(about));
+    app.route("/about", web::get().to(about))
+        .configure(view_routers);
 }
 
-pub async fn about(app_data: web::Data<AppState>) -> impl Responder {
+pub async fn about(req: HttpRequest, app_data: web::Data<AppState>) -> impl Responder {
+    let path = req.path();
+    let method = req.method();
+    tracing::info!("-->\t {method} {path}");
     #[derive(Deserialize, Serialize)]
     struct AboutResponse {
         message: String,
