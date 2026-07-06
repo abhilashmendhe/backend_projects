d7b87c6e5940788076f6d2cc3076cfad4a5b47e5
Abhilash <abhilashmendhe@gmail.com>
2026-07-06 10:31:16 +0530

    updated notify.rs. Also returning devices info

--- _013_notification_systems/notification_server/src/routes/notify/notify.rs
+++ _013_notification_systems/notification_server/src/routes/notify/notify.rs
@@ -12,9 +12,22 @@ use crate::utils::{
  *
     $ curl -X POST localhost:60001/notify \
     > -H "Content-Type: application/json" \
-    > -d '{"event_id": "evt_abc123", "recipient_user_id": "user_0042", "title": "Fall detected, Room 14", "body": "Mrs. Hansen, please check immediately", "priority": "high", "occurred_at": "2026-05-23T18:53:49Z"}'
+    > -d '{"event_id": "evt_abc123", "recipient_user_id": "user_0600", "title": "Fall detected, Room 14", "body": "Mrs. Hansen, please check immediately", "priority": "high", "occurred_at": "2026-05-23T18:53:49Z"}'
 */
 
+// --------------------- Users and Deivces combined data model --------------------
+#[derive(Debug, Deserialize)]
+pub struct UserDevice {
+    pub u_id: i64,
+    pub u_email: String,
+    pub d_id: i64,
+    pub d_device_token: String,
+    pub d_platform: String,
+    pub d_last_seen_at: Option<DateTime<Utc>>,
+    pub d_is_active: Option<bool>,
+}
+
+// --------------------- Request JSON --------------------
 #[derive(Debug, Deserialize)]
 pub struct NotificationRequest {
     pub event_id: String,
@@ -23,6 +36,14 @@ pub struct NotificationRequest {
     pub body: String,
     pub priority: String,
     pub occurred_at: DateTime<Utc>,
+}
+
+// --------------------- Response JSON --------------------
+#[derive(Debug, Serialize)]
+pub struct Platform {
+    pub platform: String,
+    pub is_active: Option<bool>,
+    pub last_seen_at: Option<DateTime<Utc>>,
 }
 
 #[derive(Debug, Serialize)]
@@ -37,6 +58,7 @@ pub struct SubNotificationResp {
 pub struct NotificationResponse {
     pub message: String,
     pub sub_notification_resp: SubNotificationResp,
+    pub platforms: Vec<Platform>,
 }
 
 pub async fn notify_event(
@@ -63,18 +85,63 @@ pub async fn notify_event(
     }
     let priority = if priority_str.eq("high") { 0 } else { 1 };
 
-    // 2. Check if user exists
-    let user_id = sqlx::query_scalar!(r#"SELECT id FROM users WHERE username=$1"#, r_user_id)
-        .fetch_one(app_data.db_pool())
-        .await
-        .map_err(|err| {
-            tracing::error!("-->\t {method} {path}  --  {:?}", err);
-            return NotificationServerErr::AppError(AppError {
-                code: StatusCode::INTERNAL_SERVER_ERROR,
-                message: "Failed to get user".to_string(),
-            });
-        })?;
-
+    // 2. Check if user and devices exists with inner join query
+    /*
+        SELECT
+            u.id AS u_id,
+            u.email AS u_email,
+            d.id AS d_id,
+            d.device_token AS d_device_token,
+            d.platform AS d_platform,
+            d.last_seen_at AS d_last_seen_at,
+            d.is_active AS d_is_active
+        FROM users AS u
+        INNER JOIN devices AS d
+        ON u.id=d.user_id WHERE u.username='user_0041';
+    */
+    let users_devices = sqlx::query_as!(
+        UserDevice,
+        r#"
+        SELECT 
+            u.id AS u_id,
+            u.email AS u_email,
+            d.id AS d_id,
+            d.device_token AS d_device_token,
+            d.platform AS d_platform,
+            d.last_seen_at AS d_last_seen_at,
+            d.is_active AS d_is_active
+        FROM users AS u 
+        INNER JOIN devices AS d 
+        ON u.id=d.user_id WHERE u.username=$1
+        "#,
+        r_user_id
+    )
+    .fetch_all(app_data.db_pool())
+    .await
+    .map_err(|err| {
+        tracing::error!("-->\t {method} {path}  --  {:?}", err);
+        return NotificationServerErr::AppError(AppError {
+            code: StatusCode::INTERNAL_SERVER_ERROR,
+            message: "Failed to get user".to_string(),
+        });
+    })?;
+    if users_devices.len() == 0 {
+        return Err(NotificationServerErr::AppError(AppError {
+            code: StatusCode::NOT_FOUND,
+            message: format!("User with id: {r_user_id} is not registered"),
+        }));
+    }
+    let mut platforms = vec![];
+    for ud in &users_devices {
+        platforms.push(
+            Platform {
+                platform: ud.d_platform.clone(),
+                is_active: ud.d_is_active ,
+                last_seen_at: ud.d_last_seen_at,
+            }
+        );
+    }
+    let user_id = users_devices[0].u_id;
     // 3. Insert data into notifications and notification_outbox table with a transaction
     let mut tx = app_data.db_pool().begin().await.map_err(|err| {
         tracing::error!("-->\t {method} {path}  --  {:?}", err);
@@ -153,5 +220,6 @@ pub async fn notify_event(
             outbox_id,
             event_id: event_id.to_string(),
         },
+        platforms,  
     }))
 }
