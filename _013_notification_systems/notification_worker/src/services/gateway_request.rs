d05682862be6d9e8cf588d769d06ace4cc200ea6
Abhilash <abhilashmendhe@gmail.com>
2026-07-10 15:25:31 +0530

    created a request to gateway.. gateway could be APN, FCM etc.

---
+++ _013_notification_systems/notification_worker/src/services/gateway_request.rs
@@ -0,0 +1,37 @@
+use reqwest::Response;
+use serde::Serialize;
+use crate::utils::error::NotificationWorkerErr;
+
+#[derive(Debug, Serialize)]
+pub struct NotifyRequest {
+    pub device_token: String,
+    pub platform: String, // ios | android | sms | web
+    pub event_id: String,
+    pub title: String,
+    pub body: String,
+    pub priority: String,     // high | normal
+    pub callback_url: String, // optional, gateway POSTs delivery confirmation here
+}
+
+impl NotifyRequest {
+    pub fn new() -> Self {
+        Self {
+            device_token: "".to_string(),
+            platform: "".to_string(),
+            event_id: "".to_string(),
+            title: "".to_string(),
+            body: "".to_string(),
+            priority: "".to_string(),
+            callback_url: "".to_string(),
+        }
+    }
+}
+
+pub async fn make_gateway_request(
+    url_gateway: String,
+    notify_req: NotifyRequest,
+) -> Result<Response, NotificationWorkerErr> {
+    let url = format!("http://{}", url_gateway);
+    let client = reqwest::Client::new();
+    Ok(client.post(url).json(&notify_req).send().await?) 
+}
