8e4b1226ed8c6c5f775de055b507781d0a1769a2
Abhilash <abhilashmendhe@gmail.com>
2026-07-10 15:24:57 +0530

    updated spawn workers

--- _013_notification_systems/notification_worker/src/services/init_workers.rs
+++ _013_notification_systems/notification_worker/src/services/init_workers.rs
@@ -1,29 +1,54 @@
 use std::sync::Arc;
 
+use sqlx::PgPool;
 use tokio::sync::{Mutex, mpsc::Receiver};
 
-use redis::streams::StreamId;
+use redis::{aio::MultiplexedConnection, streams::StreamId};
 
-pub async fn spawn_workers(_platform_spec_stream: &str, num_workers: u32, rx: Receiver<StreamId>) {
+use crate::{services::process_job::process_job, utils::error::NotificationWorkerErr};
+
+pub async fn spawn_workers(
+    priority: u8,
+    platform: String,
+    num_workers: u32,
+    url_gateway: String,
+    callback_url: String, 
+    rx: Receiver<StreamId>,
+    db_conn: PgPool,
+    q_conn: &mut MultiplexedConnection,
+) -> Result<(), NotificationWorkerErr> {
     let rx = Arc::new(Mutex::new(rx));
-    for nw in 0..num_workers {
+
+    for _ in 0..num_workers {
+        let platform = platform.clone();
         let rx = rx.clone();
+        let db_conn = db_conn.clone();
+        let url_gateway = url_gateway.clone();
+        let callback_url = callback_url.clone();
+        let q_conn = q_conn.clone();
         tokio::spawn(async move {
             loop {
-                let job = {
+                if let Some(job) = {
                     let mut rx_l = rx.lock().await;
                     rx_l.recv().await
-                };
-                if job.is_none() {
+                } {
+                    // process job
+                    // println!("worker {nw} {:?}", job);
+                    let _ = process_job(
+                        priority,
+                        platform.clone(),
+                        job,
+                        url_gateway.clone(),
+                        callback_url.clone(),
+                        db_conn.clone(),
+                        q_conn.clone(),
+                    )
+                    .await;
+                } else {
                     break;
-                }
-
-                // process job
-                println!("worker {nw} {:?}", job);
+                };
             }
-            // while let Some(streamid) = rx.recv().await {
-
-            // }
         });
     }
+    Ok(())
 }
--- _013_notification_systems/notification_worker/src/services/mod.rs
+++ _013_notification_systems/notification_worker/src/services/mod.rs
@@ -1,1 +1,3 @@
+pub mod gateway_request;
 pub mod init_workers;
+pub mod process_job;
