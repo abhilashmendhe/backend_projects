79254ad574d99b667c0b9f3edf82274f00d67763
Abhilash <abhilashmendhe@gmail.com>
2026-07-10 15:23:59 +0530

    added more errors to worker custom error handler

--- _013_notification_systems/notification_worker/src/utils/error.rs
+++ _013_notification_systems/notification_worker/src/utils/error.rs
@@ -1,4 +1,4 @@
-use std::env::VarError;
+use std::{env::VarError, num::ParseIntError};
 
 use redis::RedisError;
 use thiserror::Error;
@@ -10,4 +10,13 @@ pub enum NotificationWorkerErr {
 
     #[error("{}", 0)]
     RedisErr(#[from] RedisError),
+
+    #[error("{}", 0)]
+    ParseIntErr(#[from] ParseIntError),
+
+    #[error("{}", 0)]
+    SQLErr(#[from] sqlx::Error),
+
+    #[error("{}", 0)]
+    ReqwestErr(#[from] reqwest::Error),
 }
