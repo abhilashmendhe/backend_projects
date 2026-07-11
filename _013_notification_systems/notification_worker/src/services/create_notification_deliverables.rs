475d8462c26cdc10d9e65213d0754f32c8b4dacb
Abhilash <abhilashmendhe@gmail.com>
2026-07-11 15:05:21 +0530

    updated create notification deliverables sql command. added extra retry time param

--- _013_notification_systems/notification_worker/src/services/create_notification_deliverables.rs
+++ _013_notification_systems/notification_worker/src/services/create_notification_deliverables.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use sqlx::PgPool;
 
 use crate::utils::error::NotificationWorkerErr;
@@ -7,18 +8,20 @@ pub async fn create_notification_deliverables(
     device_id: i64,
     status: i16,
     retry_count: i16,
+    next_retry_at: Option<DateTime<Utc>>,
     db_pool: PgPool,
 ) -> Result<(), NotificationWorkerErr> {
     sqlx::query!(
         r#"
-        INSERT INTO notification_deliverables (notification_id, device_id, status, retry_count) 
+        INSERT INTO notification_deliverables (notification_id, device_id, status, retry_count, next_retry_at) 
         VALUES 
-        ($1, $2, $3, $4)
+        ($1, $2, $3, $4, $5)
         "#,
         notification_id,
         device_id,
         status,
-        retry_count
+        retry_count,
+        next_retry_at
     )
     .execute(&db_pool)
     .await?;
