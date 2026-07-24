bebf3ba9efe973040b5e3c36c11ae6015178fcc6
Abhilash <abhilashmendhe@gmail.com>
2026-07-24 22:38:36 +0530

    started working on fetching wallet info using alloy lib

--- _010_on_chain_data_acquisition/_01_wallet_tracker/src/main.rs
+++ _010_on_chain_data_acquisition/_01_wallet_tracker/src/main.rs
@@ -1,6 +1,11 @@
 use _01_wallet_tracker::{run, utils::error::WalletInfoErr};
 use clap::Parser;
 
+/*
+*  $ cargo run -- --rpc-url https://eth-mainnet.g.alchemy.com/v2 --wallets 0x165CD37b4C644C2921454429E7F9358d18A45e14
+*  $ cargo run -- --rpc-url https://rpc.ankr.com/eth --wallets 0x165CD37b4C644C2921454429E7F9358d18A45e14
+*/
+
 #[derive(Parser, Debug)]
 struct ServerCli {
     #[arg(short, long)]
@@ -25,7 +30,14 @@ async fn main() -> Result<(), WalletInfoErr> {
 
     // 1. read API KEY from .env file
     dotenv::dotenv().ok();
-    let api_key = std::env::var("RPC_API_KEY")?;
+    let alch_api_key = std::env::var("ALCHEMY_RPC_API_KEY")?;
+    let ankr_api_key = std::env::var("ANKR_RPC_API_KEY")?;
+
+    let api_key = if rpc_url.contains("ankr") {
+        ankr_api_key
+    } else {
+        alch_api_key
+    };
 
     // 2. create full rpc url
     let full_rpc_url = format!("{}/{}", rpc_url, api_key);
