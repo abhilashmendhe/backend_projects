02d25db8011313ca05b80e70a6ce25231a9ba4ff
Abhilash <abhilashmendhe@gmail.com>
2026-07-30 22:04:44 +0530

    fetching a snapshot from REST

--- _011_exchange_engine/matching_engine/src/services/fetch_rest.rs
+++ _011_exchange_engine/matching_engine/src/services/fetch_rest.rs
@@ -1,29 +1,35 @@
-use std::collections::HashMap;
-
+use crate::{
+    services::{BuffEvents, parse_string_array_to_f64},
+    utils::error::ExchangeErr,
+};
 use reqwest::StatusCode;
+use rust_decimal::Decimal;
 use serde::Deserialize;
-
-use crate::utils::error::ExchangeErr;
+use tokio::sync::mpsc::Sender;
 
 #[derive(Debug, Deserialize)]
 pub struct HttpResponseSnapshot {
     #[serde(rename = "lastUpdateId")]
     pub last_update_id: usize,
-    pub bids: Vec<[String; 2]>,
-    pub asks: Vec<[String; 2]>
+
+    #[serde(deserialize_with = "parse_string_array_to_f64")]
+    pub bids: Vec<[Decimal; 2]>,
+    #[serde(deserialize_with = "parse_string_array_to_f64")]
+    pub asks: Vec<[Decimal; 2]>,
 }
 
-pub async fn fetch_snapshot(rest_url: String) -> Result<(), ExchangeErr> {
-
+pub async fn fetch_snapshot(
+    rest_url: String,
+    sender: Sender<BuffEvents>,
+) -> Result<(), ExchangeErr> {
     let resp = reqwest::get(rest_url).await?;
     // .json::<HashMap<String, String>>()
     // .await?;
-
     if resp.status() != StatusCode::OK {
         return Err(ExchangeErr::HttpResponseErr(format!("{}", resp.status())));
     }
-    // println!("{:#?}", resp);
     let json = resp.json::<HttpResponseSnapshot>().await?;
-    println!("{:?}",json);
+    println!("{:#?}", json);
+    let _ = sender.send(BuffEvents::Snapshot(json)).await;
     Ok(())
 }
