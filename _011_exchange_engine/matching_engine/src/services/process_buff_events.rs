9ea008299c880f33fd469a6fb0552be2e55e3ff9
Abhilash <abhilashmendhe@gmail.com>
2026-08-12 21:52:15 +0530

    procssed the snapshots and ws responses

--- _011_exchange_engine/matching_engine/src/services/process_buff_events.rs
+++ _011_exchange_engine/matching_engine/src/services/process_buff_events.rs
@@ -1,8 +1,7 @@
 use tokio::sync::mpsc::{Receiver, Sender};
 
 use crate::{
-    services::{BuffEvents, fetch_rest::fetch_snapshot},
-    utils::error::ExchangeErr,
+    services::{BuffEvents, fetch_rest::{HttpResponseSnapshot, fetch_snapshot}}, utils::error::ExchangeErr,
 };
 
 pub async fn process_events(
@@ -10,26 +9,33 @@ pub async fn process_events(
     sender: Sender<BuffEvents>,
     mut receiver: Receiver<BuffEvents>,
 ) -> Result<(), ExchangeErr> {
-    let mut last_update_id = 0;
+    // let mut last_update_id = 0;
+    let mut snapshot = HttpResponseSnapshot::default();
     let mut first_b_u = 0;
     // let mut prices = vec![];
     while let Some(buff_events) = receiver.recv().await {
         // println!("{:?}", buff_events);
-        println!("last_update_id: {last_update_id}, first_b_u: {first_b_u}");
+        println!("last_update_id: {}, first_b_u: {first_b_u}", snapshot.last_update_id);
         match buff_events {
             BuffEvents::Snapshot(http_response_snapshot) => {
-                // println!("{:?}", http_response_snapshot);
-                last_update_id = http_response_snapshot.last_update_id;
+                // last_update_id = http_response_snapshot.last_update_id;
+                snapshot = http_response_snapshot;
             }
             BuffEvents::WSStream(wsresponse) => {
                 // prices.push();
                 if first_b_u <= 0 {
                     first_b_u = wsresponse.U;
-                    if last_update_id > 0 && last_update_id < first_b_u {
+                    if snapshot.last_update_id > 0 && snapshot.last_update_id < first_b_u {
                         println!("1. Not latest snapshot....");
                         fetch_snapshot(rest_url.clone(), sender.clone()).await?;
                         println!("2. Just fetched latest snapshot....\n");
                     }
+                }
+                if wsresponse.U <= snapshot.last_update_id + 1 && snapshot.last_update_id <= wsresponse.u {
+                    // println!("Found first event");
+                    // println!("{:?}", wsresponse);
+                    
+                    break;
                 }
             }
         }
