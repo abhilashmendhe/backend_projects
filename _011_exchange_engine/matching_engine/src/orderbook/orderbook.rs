74fdb67113f40b029e2ff0f28b2a9db738a73ee9
Abhilash <abhilashmendhe@gmail.com>
2026-08-13 21:50:48 +0530

    added definition for the methods

--- _011_exchange_engine/matching_engine/src/orderbook/orderbook.rs
+++ _011_exchange_engine/matching_engine/src/orderbook/orderbook.rs
@@ -3,14 +3,26 @@ use std::{cmp::Reverse, collections::BTreeMap};
 use rust_decimal::Decimal;
 
 #[derive(Debug, Clone)]
-pub struct Orderbook {  
+pub struct Orderbook {
+    pub ticker: String,
     pub last_update_id: usize,
-    pub bids: BTreeMap<Decimal, Decimal>,
-    pub asks: BTreeMap<Reverse<Decimal>, Decimal>
+    pub asks: BTreeMap<Decimal, Decimal>,
+    pub bids: BTreeMap<Reverse<Decimal>, Decimal>,
 }
 
 impl Orderbook {
-    pub fn new(last_update_id: usize) -> Self {
-        Self { last_update_id, bids: BTreeMap::new(), asks: BTreeMap::new() }
+    pub fn new(ticker: String, last_update_id: usize) -> Self {
+        Self {
+            ticker,
+            last_update_id,
+            bids: BTreeMap::new(),
+            asks: BTreeMap::new(),
+        }
     }
+
+    pub fn best_bid(&self) {}
+
+    pub fn best_ask(&self) {}
+
+    pub fn spread(&self) {}
 }
