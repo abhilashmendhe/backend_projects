ae78e7d29ecaa54e8b0de6be343fb9783485aaff
Abhilash <abhilashmendhe@gmail.com>
2026-08-13 21:50:33 +0530

    completed process_buff_events

--- _011_exchange_engine/matching_engine/src/services/process_buff_events.rs
+++ _011_exchange_engine/matching_engine/src/services/process_buff_events.rs
@@ -1,25 +1,48 @@
+use std::cmp::Reverse;
+
 use tokio::sync::mpsc::{Receiver, Sender};
 
 use crate::{
-    services::{BuffEvents, fetch_rest::{HttpResponseSnapshot, fetch_snapshot}}, utils::error::ExchangeErr,
+    orderbook::orderbook::Orderbook,
+    services::{
+        BuffEvents,
+        fetch_rest::{HttpResponseSnapshot, fetch_snapshot},
+    },
+    utils::error::ExchangeErr,
 };
 
 pub async fn process_events(
+    ticker: String,
     rest_url: String,
     sender: Sender<BuffEvents>,
     mut receiver: Receiver<BuffEvents>,
 ) -> Result<(), ExchangeErr> {
     // let mut last_update_id = 0;
     let mut snapshot = HttpResponseSnapshot::default();
+    let mut orderbook = Orderbook::new(ticker, snapshot.last_update_id);
     let mut first_b_u = 0;
+    let mut found_event = false;
     // let mut prices = vec![];
     while let Some(buff_events) = receiver.recv().await {
         // println!("{:?}", buff_events);
-        println!("last_update_id: {}, first_b_u: {first_b_u}", snapshot.last_update_id);
+        // println!("last_update_id: {}, first_b_u: {first_b_u}", snapshot.last_update_id);
         match buff_events {
             BuffEvents::Snapshot(http_response_snapshot) => {
                 // last_update_id = http_response_snapshot.last_update_id;
-                snapshot = http_response_snapshot;
+                snapshot = http_response_snapshot.clone();
+                orderbook.asks.clear();
+                orderbook.bids.clear();
+                orderbook.last_update_id = snapshot.last_update_id;
+                for ask in http_response_snapshot.asks {
+                    if !ask[1].is_zero() {
+                        orderbook.asks.insert(ask[0], ask[1]);
+                    }
+                }
+                for bid in http_response_snapshot.bids {
+                    if !bid[1].is_zero() {
+                        orderbook.bids.insert(Reverse(bid[0]), bid[1]);
+                    }
+                }
             }
             BuffEvents::WSStream(wsresponse) => {
                 // prices.push();
@@ -31,11 +54,26 @@ pub async fn process_events(
                         println!("2. Just fetched latest snapshot....\n");
                     }
                 }
-                if wsresponse.U <= snapshot.last_update_id + 1 && snapshot.last_update_id <= wsresponse.u {
-                    // println!("Found first event");
-                    // println!("{:?}", wsresponse);
-                    
-                    break;
+                if wsresponse.U <= snapshot.last_update_id + 1
+                    && snapshot.last_update_id + 1 <= wsresponse.u
+                {
+                    found_event = true;
+                }
+                if found_event {
+                    for ask in wsresponse.a {
+                        if !ask[1].is_zero() {
+                            orderbook.asks.insert(ask[0], ask[1]);
+                        } else {
+                            orderbook.asks.remove(&ask[0]);
+                        }
+                    }
+                    for bid in wsresponse.b {
+                        if !bid[1].is_zero() {
+                            orderbook.bids.insert(Reverse(bid[0]), bid[1]);
+                        } else {
+                            orderbook.bids.remove(&Reverse(bid[0]));
+                        }
+                    }
                 }
             }
         }
