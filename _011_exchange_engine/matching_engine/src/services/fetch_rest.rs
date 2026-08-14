c77c126e158d7512445f74384682bb0af1486ed7
Abhilash <abhilashmendhe@gmail.com>
2026-08-14 22:08:44 +0530

    added impl for httpsnapshot

--- _011_exchange_engine/matching_engine/src/services/fetch_rest.rs
+++ _011_exchange_engine/matching_engine/src/services/fetch_rest.rs
@@ -7,7 +7,7 @@ use rust_decimal::Decimal;
 use serde::Deserialize;
 use tokio::sync::mpsc::Sender;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct HttpResponseSnapshot {
     #[serde(rename = "lastUpdateId")]
     pub last_update_id: usize,
@@ -18,18 +18,25 @@ pub struct HttpResponseSnapshot {
     pub asks: Vec<[Decimal; 2]>,
 }
 
+impl Default for HttpResponseSnapshot {
+    fn default() -> Self {
+        Self {
+            last_update_id: Default::default(),
+            bids: Default::default(),
+            asks: Default::default(),
+        }
+    }
+}
+
 pub async fn fetch_snapshot(
     rest_url: String,
     sender: Sender<BuffEvents>,
 ) -> Result<(), ExchangeErr> {
     let resp = reqwest::get(rest_url).await?;
-    // .json::<HashMap<String, String>>()
-    // .await?;
     if resp.status() != StatusCode::OK {
         return Err(ExchangeErr::HttpResponseErr(format!("{}", resp.status())));
     }
     let json = resp.json::<HttpResponseSnapshot>().await?;
-    println!("{:#?}", json);
     let _ = sender.send(BuffEvents::Snapshot(json)).await;
     Ok(())
 }
