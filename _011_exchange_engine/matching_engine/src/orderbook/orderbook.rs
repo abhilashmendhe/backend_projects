90bce770f972adc66b8cdd6dc027c11cb4561330
Abhilash <abhilashmendhe@gmail.com>
2026-08-17 22:08:47 +0530

    implmented spread method for ordrebook

--- _011_exchange_engine/matching_engine/src/orderbook/orderbook.rs
+++ _011_exchange_engine/matching_engine/src/orderbook/orderbook.rs
@@ -20,9 +20,20 @@ impl Orderbook {
         }
     }
 
-    pub fn best_bid(&self) {}
+    pub fn best_bid(&self) -> Option<(&Reverse<Decimal>, &Decimal)> {
+        self.bids.first_key_value()
+    }
 
-    pub fn best_ask(&self) {}
+    pub fn best_ask(&self) -> Option<(&Decimal, &Decimal)> {
+        self.asks.first_key_value()
+    }
 
-    pub fn spread(&self) {}
+    pub fn spread(&self) -> Option<Decimal> {
+        if let Some((ask_price, _ask_quantity)) = self.best_ask() {
+            if let Some((bid_price, _bid_quantity)) = self.best_ask() {
+                return Some(*ask_price-bid_price);
+            }
+        }
+        None
+    }
 }
