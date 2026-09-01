15a215d395d490cf7fb760bf28dab6f51f3e26a6
Abhilash <abhilashmendhe@gmail.com>
2026-09-01 15:53:53 +0530

    reformatted file..

--- _014_real_time_streaming/streaming_services/src/ingestion_service/start_server.rs
+++ _014_real_time_streaming/streaming_services/src/ingestion_service/start_server.rs
@@ -1,18 +1,27 @@
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 use serde::Serialize;
 
-use crate::utils::errors::IngestionServiceErr;
+use crate::{
+    utils::{app_state::AppState, errors::IngestionServiceErr},
+    view_router::views_factory,
+};
 
 pub async fn start_server(
     server_workers: usize,
     server_addr: &str,
     port: u16,
+    app_state: web::Data<AppState>,
 ) -> Result<(), IngestionServiceErr> {
-    HttpServer::new(move || App::new().default_service(web::route().to(not_found)))
-        .workers(server_workers)
-        .bind(format!("{}:{}", server_addr, port))?
-        .run()
-        .await?;
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app_state.clone())
+            .default_service(web::route().to(not_found))
+            .configure(views_factory)
+    })
+    .workers(server_workers)
+    .bind(format!("{}:{}", server_addr, port))?
+    .run()
+    .await?;
 
     Ok(())
 }
