e06232ab69c0612087b8d3b8fc1e64f54f8a0e87
Abhilash <abhilashmendhe@gmail.com>
2026-09-01 15:55:13 +0530

    reformatted main. Added more cli params for grpc addr and port

--- _014_real_time_streaming/streaming_services/src/ingestion_service/main.rs
+++ _014_real_time_streaming/streaming_services/src/ingestion_service/main.rs
@@ -1,14 +1,27 @@
+use actix_web::web;
 use clap::Parser;
 use tracing::level_filters::LevelFilter;
 
-use crate::{start_server::start_server, utils::errors::IngestionServiceErr};
+use crate::{
+    start_server::start_server,
+    stream_service::message_stream_client::MessageStreamClient,
+    utils::{app_state::AppState, config::Config, errors::IngestionServiceErr},
+};
 
+pub mod models;
+pub mod routes;
 pub mod start_server;
 pub mod utils;
+pub mod view_router;
 
+pub mod stream_service {
+    tonic::include_proto!("stream_service");
+}
 /*
     To run:
-    $ cargo run --bin ingestion_service -- --addr localhost --port 8000
+    $ cargo run --bin ingestion_service -- --addr localhost --port 8080
+
+    $ cargo watch  -q -c -w src/ingestion_service -x "run -- --addr localhost -p 8080"
 */
 
 #[derive(Debug, Parser)]
@@ -21,25 +34,46 @@ struct ServerCli {
 
     #[arg(short, long, default_value_t = 4)]
     server_workers: usize,
+
+    #[arg(long)]
+    grpc_addr: String,
+
+    #[arg(long)]
+    grpc_port: u16,
 }
 
 #[actix_web::main]
 async fn main() -> Result<(), IngestionServiceErr> {
-    println!("I am ingestion service!");
-
     // 1. Parse command line
     let scli = ServerCli::parse();
     let server_addr = scli.addr;
     let port = scli.port;
     let server_workers = scli.server_workers;
+    let grpc_addr = scli.grpc_addr;
+    let grpc_port = scli.grpc_port;
 
     // 2. Enable tracing
     tracing_subscriber::fmt()
         .with_max_level(LevelFilter::DEBUG)
         .init();
+    tracing::info!("I am ingestion service");
 
-    // 3. Spin a actix server
-    start_server(server_workers, &server_addr, port).await?;
+    // 3. Configure grpc channel
+    let grpc_full_addr = format!("http://{}:{}", grpc_addr, grpc_port);
+    // println!("{}",grpc_full_addr);
+    let grpc_channel = tonic::transport::Endpoint::from_shared(grpc_full_addr)?
+        .connect()
+        .await?;
+
+    // 4. Create a client
+    let client = MessageStreamClient::new(grpc_channel);
+
+    // 5. Configure config and app state
+    let config = Config::new();
+    let app_state = web::Data::new(AppState::new(config, client));
+
+    // 6. Spin a actix server
+    start_server(server_workers, &server_addr, port, app_state).await?;
 
     Ok(())
 }
